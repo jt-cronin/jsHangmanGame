a19b7e6aea778e962312a33b64a405bdedfbe46c
John T Cronin <jt.cronin@outlook.com>
2017-09-18 13:30:52 -0500

    messed up, need to make new repo

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 zipCode = process.argv[2]
 console.log("We will look up the wather for your zipCode...")
 console.log(zipCode)
+
+
+var http = require('http');
+
+var request = new XMLHttpRequest();
+
+request.open("GET", "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA", true);
+
+console.log(request)
